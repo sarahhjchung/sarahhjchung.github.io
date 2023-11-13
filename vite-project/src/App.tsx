7054b9bffeaf3482f7d86600afbac99a521ac529
Sarah Chung <sarahc0304@gmail.com>
2023-11-12 21:46:07 -0500

    Set up project

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <h2>Hello world!</h2>
+      <h3>Hello world!</h3>
+    </>
+  );
+}
+
+export default App;
