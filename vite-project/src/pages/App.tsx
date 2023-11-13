547006c1f37cdb587ff952bac15fb64d6840334a
Sarah Chung <sarahc0304@gmail.com>
2023-11-12 21:57:00 -0500

    Set up tailwind

--- vite-project/src/pages/App.tsx
+++ vite-project/src/pages/App.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 export function App() {
   return (
     <>
-      <h1>Hello world!</h1>
-      <h2>Hello world!</h2>
-      <h3>Hello world!</h3>
+      <h1 className="text-5xl">Hello world!</h1>
+      <h2 className="text-4xl">Hello world!</h2>
+      <h3 className="text-2xl">Hello world!</h3>
     </>
   );
 }
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1400px",
+      },
+    },
+    fontFamily: {
+      serif: ["Hahmlet", "serif"],
+      sans: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        background: "#FFFCFA",
+        text: "#0B0805",
+        primary: "#076BCF",
+        secondary: "#FCD1A6",
+        accent: "#243956",
+      },
+    },
   },
   plugins: [],
 };
