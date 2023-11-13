f91d743a883f900ae1005112623b3a810ad1848e
Sarah Chung <sarahc0304@gmail.com>
2023-11-13 00:36:15 -0500

    Basic structure of home page

--- vite-project/src/pages/App.tsx
+++ vite-project/src/pages/App.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 
 import Strings from "../locales/en.json";
-import { Button } from "@/components/ui/button";
+import { MenuSquare } from "lucide-react";
+import logoSVG from "@/assets/logo.svg";
+import deskSVG from "@/assets/desk.svg";
 
 export function App() {
   return (
-    <div className="container px-8 md:px-40 flex flex-col gap-y-8 items-center justify-center">
-      <h1 className="text-5xl text-center">{Strings.Main.title}</h1>
-      <h4 className="text-2xl text-center">
-        I’m a <span className="font-bold">Computer Science Specialist</span>{" "}
-        student at the <span className="font-bold">University of Toronto </span>
-        . I’m striving to become a{" "}
-        <span className="font-bold">creative developer and designer</span>.
-      </h4>
-      <Button>Click me</Button>
+    <div className="container py-12 md:px-40 flex flex-col gap-y-12 items-center justify-center">
+      <div className="flex flex-row w-full justify-between">
+        <img src={logoSVG} alt="Logo" className="logo" />
+        <MenuSquare className="menu" />
+      </div>
+      <div className="flex flex-col gap-y-8 md:w-5/6">
+        <h1>{Strings.Main.title}</h1>
+        <h4 className="subtitle">
+          I’m a <span className="font-bold">Computer Science Specialist</span>{" "}
+          student at the{" "}
+          <span className="font-bold">University of Toronto</span>. I’m striving
+          to become a{" "}
+          <span className="font-bold">creative developer and designer</span>.
+        </h4>
+      </div>
+
+      <img src={deskSVG} alt="Desk Illustration" />
     </div>
   );
 }
