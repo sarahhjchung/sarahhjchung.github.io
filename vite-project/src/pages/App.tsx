398602c4d77ddaa861b25873427c1d71156ee6a5
Sarah Chung <sarahc0304@gmail.com>
2023-11-13 00:04:54 -0500

    Set up shadcn

--- vite-project/src/pages/App.tsx
+++ vite-project/src/pages/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+import Strings from "../locales/en.json";
+import { Button } from "@/components/ui/button";
+
 export function App() {
   return (
-    <>
-      <h1 className="text-5xl">Hello world!</h1>
-      <h2 className="text-4xl">Hello world!</h2>
-      <h3 className="text-2xl">Hello world!</h3>
-    </>
+    <div className="container px-8 md:px-40 flex flex-col gap-y-8 items-center justify-center">
+      <h1 className="text-5xl text-center">{Strings.Main.title}</h1>
+      <h4 className="text-2xl text-center">
+        I’m a <span className="font-bold">Computer Science Specialist</span>{" "}
+        student at the <span className="font-bold">University of Toronto </span>
+        . I’m striving to become a{" "}
+        <span className="font-bold">creative developer and designer</span>.
+      </h4>
+      <Button>Click me</Button>
+    </div>
   );
 }
