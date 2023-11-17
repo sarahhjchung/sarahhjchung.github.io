6e7b875005f116cabdf372f62d7d7fcc8039ef24
Sarah Chung <sarahc0304@gmail.com>
2023-11-17 00:44:17 -0500

    Make contact links work

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,5 +1,6 @@
 import Strings from "@/locales/en.json";
 import contactSVG from "@/assets/contact.svg";
+import resumePDF from "@/assets/resume.pdf";
 import { FileText, Github, Linkedin, Mail } from "lucide-react";
 
 export function Contact() {
@@ -11,26 +12,37 @@ export function Contact() {
         className="absolute bottom-0 h-3/4 opacity-70"
       />
       <h1>{Strings.Contact.title}</h1>
-      <div className="flex flex-col pt-36 gap-y-16 md:w-5/6 items-center justify-center">
+      <div className="z-10 flex flex-col pt-36 gap-y-16 md:w-5/6 items-center justify-center">
         <div className="flex flex-row gap-x-24 justify-center items-center">
-          <div className="cursor-pointer hover:stroke-primary hover:text-primary">
-            <FileText className="h-16 w-16 stroke-1" />
-            <p className="subtitle">Resume</p>
-          </div>
-          <div>
-            <Mail className="h-16 w-16 stroke-1" />
-            <p className="subtitle">Email</p>
-          </div>
+          <a href={resumePDF} target="_blank">
+            <div className="cursor-pointer hover:stroke-primary hover:text-primary">
+              <FileText className="h-16 w-16 stroke-1" />
+              <p className="subtitle">Resume</p>
+            </div>
+          </a>
+          <a href="mailto: sarahc0304@gmail.com" target="_blank">
+            <div className="cursor-pointer hover:stroke-primary hover:text-primary">
+              <Mail className="h-16 w-16 stroke-1" />
+              <p className="subtitle">Email</p>
+            </div>
+          </a>
         </div>
         <div className="flex flex-row gap-x-24 justify-center items-center">
-          <div>
-            <Linkedin className="h-16 w-16 stroke-1" />
-            <p className="subtitle">LinkedIn</p>
-          </div>
-          <div>
-            <Github className="h-16 w-16 stroke-1" />
-            <p className="subtitle">Github</p>
-          </div>
+          <a
+            href="https://www.linkedin.com/in/sarah-chung-59a923202/"
+            target="_blank"
+          >
+            <div className="cursor-pointer hover:stroke-primary hover:text-primary">
+              <Linkedin className="h-16 w-16 stroke-1" />
+              <p className="subtitle">LinkedIn</p>
+            </div>
+          </a>
+          <a href="https://github.com/sarahhjchung" target="_blank">
+            <div className="cursor-pointer hover:stroke-primary hover:text-primary">
+              <Github className="h-16 w-16 stroke-1" />
+              <p className="subtitle">Github</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
