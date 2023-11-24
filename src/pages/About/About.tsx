ed5fa704997647ce13e91cda848eda6650cd7aa3
Sarah Chung <sarahc0304@gmail.com>
2023-11-24 16:59:25 -0500

    Finish About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,5 @@
 import Strings from "@/locales/en.json";
 import aboutSVG from "@/assets/about.svg";
-import techinternPNG from "@/assets/techintern.png";
-import hatcheryPNG from "@/assets/hatchery.png";
-import wattpadPNG from "@/assets/wattpad.png";
-import { MoveRight } from "lucide-react";
 
 export function About() {
   return (
@@ -14,22 +10,12 @@ export function About() {
         className="absolute bottom-0 w-3/5 opacity-70"
       />
       <h1>{Strings.About.title}</h1>
-      <div className="flex flex-col items-center justify-center gap-y-8 md:w-5/6">
-        {/* <h3>{Strings.About.myPassionsTitle}</h3>
-        <p className="text-center">{Strings.About.passions}</p> */}
+      <div className="z-10 flex flex-col items-center justify-center gap-y-8 rounded-md bg-background/50 p-2 md:w-3/4">
+        <p className="text-center">{Strings.About.passions1}</p>
+        <p className="text-center">{Strings.About.passions2}</p>
         <h3>{Strings.About.myWorkExperienceTitle}</h3>
-        <p>{Strings.About.workExperience}</p>
-        <div className="flex flex-row items-center justify-center gap-x-4">
-          <img
-            src={techinternPNG}
-            alt="Techintern"
-            className="rounded-md h-12"
-          />
-          <MoveRight />
-          <img src={hatcheryPNG} alt="Hatchery" className="rounded-md h-12" />
-          <MoveRight />
-          <img src={wattpadPNG} alt="Wattpad" className="rounded-md h-12" />
-        </div>
+        <p className="text-center">{Strings.About.workExperience1}</p>
+        <p className="text-center">{Strings.About.workExperience2}</p>
       </div>
     </div>
   );
